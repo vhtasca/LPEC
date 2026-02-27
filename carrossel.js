2e49df2d72a9ca66e89945dfc2d782bc906215d4
alannaaaa <alanna.marchetti2014@gmail.com>
2026-02-27 17:06:09 -0300

    Código refatorado, footer, background, etc...

--- carrossel.js
+++ carrossel.js
@@ -42,3 +42,27 @@ window.addEventListener("scroll", () => {
   lastScroll = currentScroll;
 
 });
+
+
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll(".botao a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  links.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
